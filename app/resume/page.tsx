dbf47bcba18fdfdae4b88ade659b7c93766b9c67
Abdullah Ayman <abdullahaymanmsre@gmail.com>
2024-07-30 13:03:22 +0300

    finished most parts in resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,7 +8,22 @@ import {
   FaPhp,
   FaLaravel,
 } from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs, SiMysql } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiMysql,
+  SiTypescript,
+} from "react-icons/si";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 
 const about = {
   title: "About Me",
@@ -46,7 +61,7 @@ const experience = {
   icon: "/assets/resume/badge.svg",
   title: "My Experience",
   description:
-    "I have worked on a variety of projects, including e-commerce websites, blogs, and personal portfolios. I have experience with both front-end and back-end development, and I am comfortable working with a variety of technologies.",
+    "I have worked on a variety of projects, including educational websites, blogs, and personal portfolios. I have experience with both front-end and back-end development, and I am comfortable working with a variety of technologies.",
   items: [
     {
       company: "Wearby",
@@ -101,6 +116,10 @@ const skills = {
       name: "react",
     },
     {
+      icon: <SiTypescript />,
+      name: "typescript",
+    },
+    {
       icon: <SiNextdotjs />,
       name: "next.js",
     },
@@ -124,5 +143,138 @@ const skills = {
 };
 
 export default function Resume() {
-  return <div>Hi</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 1.6, duration: 0.4, ease: "easeIn" },
+      }}
+      className="flex min-h-[80vh] justify-center py-12 xl:py-8"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          className="flex flex-col gap-[60px] xl:flex-row"
+          defaultValue="experience"
+        >
+          <TabsList className="mx-auto flex w-full max-w-[380px] flex-col gap-6 xl:mx-0">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-7 max-xl:text-center">
+                <TitleAndDescription
+                  title={experience.title}
+                  description={experience.description}
+                />
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 gap-7 lg:grid-cols-2">
+                    {experience.items.map((item) => (
+                      <li
+                        key={item.company}
+                        className="flex h-[184px] flex-col items-center justify-center gap-1 rounded-xl bg-[#232329] px-10 py-6 lg:items-start"
+                      >
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="min-h-[60px] text-center text-xl lg:text-left">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          {/* dot */}
+                          <span className="size-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-7 max-xl:text-center">
+                <TitleAndDescription
+                  title={education.title}
+                  description={education.description}
+                />
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 gap-7 lg:grid-cols-2">
+                    {education.items.map((item) => (
+                      <li
+                        key={item.institution}
+                        className="flex h-[184px] flex-col items-center justify-center gap-1 rounded-xl bg-[#232329] px-10 py-6 lg:items-start"
+                      >
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="min-h-[60px] text-center text-xl lg:text-left">
+                          {item.degree}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          {/* dot */}
+                          <span className="size-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.institution}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="skills" className="w-full">
+              <div className="flex flex-col gap-7">
+                <div className="flex flex-col gap-7 max-xl:text-center">
+                  <TitleAndDescription
+                    title={skills.title}
+                    description={skills.description}
+                  />
+                </div>
+                <ul className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 xl:gap-7">
+                  {skills.skillList.map((skill) => (
+                    <li key={skill.name}>
+                      <TooltipProvider delayDuration={100}>
+                        <Tooltip>
+                          <TooltipTrigger className="group flex h-[150px] w-full items-center justify-center rounded-xl bg-[#232329]">
+                            <div className="text-6xl transition-all duration-300 group-hover:text-accent">
+                              {skill.icon}
+                            </div>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p className="capitalize">{skill.name}</p>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="about" className="w-full">
+              Coming soon!
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
+}
+
+function TitleAndDescription({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <>
+      <h3 className="text-4xl font-bold">{title}</h3>
+      <p className="mx-auto max-w-[600px] text-white/60 xl:mx-0">
+        {description}
+      </p>
+    </>
+  );
 }
--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Tabs = TabsPrimitive.Root
+const Tabs = TabsPrimitive.Root;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
@@ -13,14 +13,11 @@ const TabsList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
-    className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-slate-100 p-1 text-slate-500 dark:bg-slate-800 dark:text-slate-400",
-      className
-    )}
+    className={cn("inline-flex h-auto rounded-md p-1 text-primary", className)}
     {...props}
   />
-))
-TabsList.displayName = TabsPrimitive.List.displayName
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
@@ -29,13 +26,13 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-950 data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50",
-      className
+      "inline-flex w-full items-center justify-center whitespace-nowrap rounded-lg bg-[#27272c] p-3 text-base font-medium text-white ring-offset-white transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-accent data-[state=active]:font-bold data-[state=active]:text-primary data-[state=active]:shadow-sm",
+      className,
     )}
     {...props}
   />
-))
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
 const TabsContent = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Content>,
@@ -44,12 +41,12 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
-      className
+      "min-h-[480px] ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+      className,
     )}
     {...props}
   />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
+));
+TabsContent.displayName = TabsPrimitive.Content.displayName;
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
